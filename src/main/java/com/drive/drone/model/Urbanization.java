84560e870acbef6cb693c6d8d4ea46fa67e1ecb0
gvicedo <gvicedo90@gmail.com>
2018-06-06 18:25:01 +0200

    Remove unused values

--- src/main/java/com/drive/drone/model/Urbanization.java
+++ src/main/java/com/drive/drone/model/Urbanization.java
@@ -8,12 +8,6 @@ public class Urbanization {
     /** The id. */
     private String id;
     
-    /** The coord X. */
-    private String coordX;
-    
-    /** The coord Y. */
-    private String coordY;
-    
     /**
      * Gets the id.
      *
@@ -31,44 +25,6 @@ public class Urbanization {
     public void setId(
         String id){
         this.id = id;
-    }
-    
-    /**
-     * Gets the coord X.
-     *
-     * @return the coord X
-     */
-    public String getCoordX(){
-        return coordX;
-    }
-    
-    /**
-     * Sets the coord X.
-     *
-     * @param coordX the new coord X
-     */
-    public void setCoordX(
-        String coordX){
-        this.coordX = coordX;
-    }
-    
-    /**
-     * Gets the coord Y.
-     *
-     * @return the coord Y
-     */
-    public String getCoordY(){
-        return coordY;
-    }
-    
-    /**
-     * Sets the coord Y.
-     *
-     * @param coordY the new coord Y
-     */
-    public void setCoordY(
-        String coordY){
-        this.coordY = coordY;
     }
     
 }
